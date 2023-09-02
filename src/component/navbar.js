881fbb6ee1557fe62de37027bbf8bbdd7eeb2cd7
HKB <baba@HKB-MacBook-Pro.local>
2023-09-02 22:49:47 +0300

    created

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [navActive, setNavActive] = useState(false);
+
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
+
   return (
     <div>
       <header>
-    <nav class="navbar">
-      <div class="logo">
-        <a href="#">TVDB</a>
-      </div>
-      <ul class="links">
-        <li><a href="#">Filmler</a></li>
-        <li><a href="#">Diziler</a></li>
-        <li><a href="#">Kisiler </a></li>
-        <li><a href="#">Daha fazla</a></li>
-      </ul>
-      <div class="box">
-    <form name="search">
-        <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();"/>
-    </form>
-    <i class="fas fa-search"></i>
-
-</div>
-    </nav>
-  </header>
+        <nav className={`navbar ${navActive ? "nav-active" : ""}`}>
+          <div className="logo">
+            <a href="/">TVDB</a>
+          </div>
+          <ul className="links">
+            <li>
+              <a href="/">Filmler</a>
+            </li>
+            <li>
+              <a href="/">Diziler</a>
+            </li>
+            <li>
+              <a href="/">Kişiler</a>
+            </li>
+            <li>
+              <a href="/">Daha fazla</a>
+            </li>
+          </ul>
+          <div className="box">
+            <form name="search">
+              <input
+                type="text"
+                className="input"
+                name="txt"
+                onMouseOut={(e) => {
+                  e.target.value = "";
+                  e.target.blur();
+                }}
+              />
+            </form>
+            <i className="fas fa-search"></i>
+          </div>
+          <div
+            className={`burger ${navActive ? "close" : ""}`}
+            onClick={toggleNav}
+          >
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 };
