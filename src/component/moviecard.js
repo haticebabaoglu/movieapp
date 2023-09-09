93c1b9ef539650402cd4ab823e05b20659fccc88
haticebabaoglu <haticebaba63@gmail.com>
2023-09-09 12:15:57 +0300

    update

--- src/component/moviecard.js
+++ src/component/moviecard.js
@@ -3,12 +3,7 @@ import React from "react";
 const Moviecard = () => {
   return (
     <div>
-      <div className="moviecard">
-        <img
-          src="{https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.beyazperde.com%2Ffilmler%2Ffilm-296168%2F&psig=AOvVaw1HtEPavKUl3XfUfDXen2vc&ust=1694318256389000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCND2vsDRnIEDFQAAAAAdAAAAABAE}"
-          alt=""
-        />
-      </div>
+      <div className="moviecard"></div>
     </div>
   );
 };
