e672fec5373657c914f1f13ef9f95933021d93af
haticebabaoglu <haticebaba63@gmail.com>
2023-09-10 11:37:22 +0300

    uptade

--- src/component/moviecard.js
+++ src/component/moviecard.js
@@ -3,7 +3,9 @@ import React from "react";
 const Moviecard = () => {
   return (
     <div>
-      <div className="moviecard"></div>
+      <div className="moviecard">
+        <img src="src/logo512.png" alt="" />
+      </div>
     </div>
   );
 };
