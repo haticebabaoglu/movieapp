97faaa73d45c2ce03fa7c97ce997937dd88f94d8
haticebabaoglu <haticebaba63@gmail.com>
2023-10-09 14:16:38 +0300

    updated api fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import Navbar from "./component/navbar";
+import Moviecard from "./component/moviecard";
+import Hero from "./component/hero";
 import Movie from "./component/Movie";
 
 function App() {
   return (
     <div>
+      <Navbar />
+      <Moviecard />
+      <Hero />
       <Movie />
     </div>
   );
--- src/component/Movie.js
+++ src/component/Movie.js
@@ -2,31 +2,56 @@ import React, { useEffect, useState } from "react";
 
 const Movie = () => {
   const [movielist, setMovielist] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const apiKey = `f7c19e2a1ebb717261ebdf4edc446677`; 
 
-  const getMovie = () => {
-
-    fetch("https://api.themoviedb.org/3/trending/movie", {
-      method: 'GET',
-      headers: {
-            'Authorization': 'Bearer: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2MxOWUyYTFlYmI3MTcyNjFlYmRmNGVkYzQ0NjY3NyIsInN1YiI6IjY1MjFiMDg3MDcyMTY2MDExYzA1NjFiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t5n4TymD3RPSLFw263tBjHLGv2tfOi0Dgx0iQHzcClA',
-                Accept: "application/json",
-                "Content=Type": "application/json"
-            },
-    })
-  };
   useEffect(() => {
-    getMovie()
-  }, []);
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
+        );
 
+        if (!response.ok) {
+          throw new Error("API isteği başarısız oldu.");
+        }
+
+        const data = await response.json();
+        setMovielist(data.results);
+        setLoading(false);
+      } catch (error) {
+        setError("Veriler alınamadı. Lütfen daha sonra tekrar deneyin.");
+        setLoading(false);
+        console.error("API hatası:", error);
+      }
+    };
+
+    fetchMovies();
+  }, [apiKey]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <div>
       {movielist.map((movie) => (
-        <img src={"https://image.tmdb.org/t/p/w500/${movie.poster_path}"} />
+        <div key={movie.id} className="movie">
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt={movie.title}
+            style={{ width: 350, height: 350 }}
+          />
+          <h2>{movie.title}</h2>
+        </div>
       ))}
     </div>
-  )
-
+  );
 };
 
 export default Movie;
