151bf5eda8d2d046b63cc81f1fbcc9c2503ff6c4
haticebabaoglu <haticebaba63@gmail.com>
2023-10-22 21:55:53 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import Navbar from "./component/navbar";
 import Hero from "./component/PopularSection";
 import Movie from "./component/Movie";
+import Layout from "./component/Layout";
 
 function App() {
 
   
   return (
     <div>
-
+      <Layout/>
       <Navbar />
       <Movie/>
       <Hero />
