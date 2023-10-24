d4093561e0574cfcd3ead647238a50aa1df87bb1
haticebabaoglu <haticebaba63@gmail.com>
2023-10-25 01:19:27 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 
 import Navbar from "./component/navbar";
-import Hero from "./component/PopularSection";
 import Movie from "./component/Movie";
 import Layout from "./component/Layout";
+import MovieList from "./component/MovieList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 
 function App() {
 
   
   return (
     <div>
-
       <Router>
           <Header />
             <Routes>
-                <Route index element={<Home />}></Route>
-                <Route path="movie/:id" element={<Movie />}></Route>
-                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route index element={<Home/>}></Route>
+                <Route path="/" element={<Movie />}></Route>
+                <Route path="/" element={<MovieList />}></Route>
                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
             </Routes>
         </Router>
