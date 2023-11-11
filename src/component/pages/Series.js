df085387b9056a03c9c81857b87e5dc6ae884afe
haticebabaoglu <haticebaba63@gmail.com>
2023-11-11 17:44:16 +0300

    update

--- src/component/pages/Series.js
+++ src/component/pages/Series.js
@@ -1,1 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
 
+const Series = () => {
+  return (
+    <div>
+      <NavLink to="/home" element={<Home />}>
+        {" "}
+      </NavLink>
+    </div>
+  );
+};
+
+export default Series;
